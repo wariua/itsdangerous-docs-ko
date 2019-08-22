9c35d59a0063e2cb020942ea727f4c2545f6f1f2
Seonghun Lim <wariua@gmail.com>
2019-08-22 21:56:28 +0900

    translated timed.py

--- srcs/1.1.x/src/itsdangerous/timed.py
+++ srcs/1.1.x/src/itsdangerous/timed.py
@@ -15,26 +15,26 @@ from .signer import Signer
 
 
 class TimestampSigner(Signer):
-    """Works like the regular :class:`.Signer` but also records the time
-    of the signing and can be used to expire signatures. The
-    :meth:`unsign` method can raise :exc:`.SignatureExpired` if the
-    unsigning failed because the signature is expired.
+    """기본 :class:`.Signer`\처럼 동작하되 서명 시간까지
+    기록하므로 서명을 만료되게 할 수 있다. 서명이 만료돼서
+    서명 검증에 실패하면 :meth:`unsign` 메소드에서
+    :exc:`.SignatureExpired`\를 던질 수 있다.
     """
 
     def get_timestamp(self):
-        """Returns the current timestamp. The function must return an
-        integer.
+        """현재 타임스탬프를 반환한다. 반드시 정수를 반환해야
+        한다.
         """
         return int(time.time())
 
     def timestamp_to_datetime(self, ts):
-        """Used to convert the timestamp from :meth:`get_timestamp` into
-        a datetime object.
+        """:meth:`get_timestamp`\에서 얻은 타임스탬프를
+        datetime 객체로 변환하는 데 사용.
         """
         return datetime.utcfromtimestamp(ts)
 
     def sign(self, value):
-        """Signs the given string and also attaches time information."""
+        """문자열을 받아서 서명을 하고 시간 정보를 덧붙인다."""
         value = want_bytes(value)
         timestamp = base64_encode(int_to_bytes(self.get_timestamp()))
         sep = want_bytes(self.sep)
@@ -42,11 +42,10 @@ class TimestampSigner(Signer):
         return value + sep + self.get_signature(value)
 
     def unsign(self, value, max_age=None, return_timestamp=False):
-        """Works like the regular :meth:`.Signer.unsign` but can also
-        validate the time. See the base docstring of the class for
-        the general behavior. If ``return_timestamp`` is ``True`` the
-        timestamp of the signature will be returned as a naive
-        :class:`datetime.datetime` object in UTC.
+        """기본 :meth:`.Signer.unsign``\과 비슷하게 동작하되 시간도
+        검사한다. 기본 동작 방식은 기반 클래스의 docstring을 보라.
+        ``return_timestamp``\가 ``True``\면 서명의 타임스탬프를
+        UTC로 된 :class:`datetime.datetime` 객체로 반환한다.
         """
         try:
             result = Signer.unsign(self, value)
@@ -99,8 +98,8 @@ class TimestampSigner(Signer):
         return value
 
     def validate(self, signed_value, max_age=None):
-        """Only validates the given signed value. Returns ``True`` if
-        the signature exists and is valid."""
+        """서명된 값을 받아서 검증만 한다. 서명이 존재하고 유효하면
+        ``True``\를 반환한다."""
         try:
             self.unsign(signed_value, max_age=max_age)
             return True
@@ -109,19 +108,18 @@ class TimestampSigner(Signer):
 
 
 class TimedSerializer(Serializer):
-    """Uses :class:`TimestampSigner` instead of the default
-    :class:`.Signer`.
+    """기본으로 :class:`.Signer` 대신 :class:`TimestampSigner`\를
+    쓴다.
     """
 
     default_signer = TimestampSigner
 
     def loads(self, s, max_age=None, return_timestamp=False, salt=None):
-        """Reverse of :meth:`dumps`, raises :exc:`.BadSignature` if the
-        signature validation fails. If a ``max_age`` is provided it will
-        ensure the signature is not older than that time in seconds. In
-        case the signature is outdated, :exc:`.SignatureExpired` is
-        raised. All arguments are forwarded to the signer's
-        :meth:`~TimestampSigner.unsign` method.
+        """:meth:`dumps`\의 반대. 서명 검증이 실패하면
+        :exc:`.BadSignature`\를 던진다. ``max_age``\를 주면 서명이
+        그 초 수보다 오래됐는지 확인한다. 서명이 그보다 오래된 경우
+        :exc:`.SignatureExpired`\를 던진다. 모든 인자들이 signer의
+        :meth:`~TimestampSigner.unsign` 메소드로 전달된다.
         """
         s = want_bytes(s)
         last_exception = None
