a54a8c6fc5cf8906935fe59bdf664763b4394fcc
Seonghun Lim <wariua@gmail.com>
2019-08-24 09:17:11 +0900

    translated jws.py

--- srcs/1.1.x/src/itsdangerous/jws.py
+++ srcs/1.1.x/src/itsdangerous/jws.py
@@ -19,8 +19,8 @@ from .signer import NoneAlgorithm
 
 
 class JSONWebSignatureSerializer(Serializer):
-    """This serializer implements JSON Web Signature (JWS) support. Only
-    supports the JWS Compact Serialization.
+    """이 직렬화 모듈에선 JSON 웹 서명(JWS) 지원을 구현하고 있다.
+    JWS Compact Serialization만 지원한다.
     """
 
     jws_algorithms = {
@@ -30,7 +30,7 @@ class JSONWebSignatureSerializer(Serializer):
         "none": NoneAlgorithm(),
     }
 
-    #: The default algorithm to use for signature generation
+    #: 서명 생성에 사용할 기본 알고리듬
     default_algorithm = "HS512"
 
     default_serializer = _CompactJSON
@@ -127,17 +127,17 @@ class JSONWebSignatureSerializer(Serializer):
         return header
 
     def dumps(self, obj, salt=None, header_fields=None):
-        """Like :meth:`.Serializer.dumps` but creates a JSON Web
-        Signature. It also allows for specifying additional fields to be
-        included in the JWS header.
+        """:meth:`.Serializer.dumps`\와 비슷하되 JSON 웹 서명을
+        만들어 낸다. JWS 헤더에 추가로 포함시킬 필드를 지정하는 것도
+        가능하다.
         """
         header = self.make_header(header_fields)
         signer = self.make_signer(salt, self.algorithm)
         return signer.sign(self.dump_payload(header, obj))
 
     def loads(self, s, salt=None, return_header=False):
-        """Reverse of :meth:`dumps`. If requested via ``return_header``
-        it will return a tuple of payload and header.
+        """:meth:`dumps`\의 반대. ``return_header``\를 설정해
+        요청하면 페이로드와 헤더로 된 튜플을 반환한다.
         """
         payload, header = self.load_payload(
             self.make_signer(salt, self.algorithm).unsign(want_bytes(s)),
@@ -155,15 +155,14 @@ class JSONWebSignatureSerializer(Serializer):
 
 
 class TimedJSONWebSignatureSerializer(JSONWebSignatureSerializer):
-    """Works like the regular :class:`JSONWebSignatureSerializer` but
-    also records the time of the signing and can be used to expire
-    signatures.
+    """기본 :class:`JSONWebSignatureSerializer`\처럼 동작하되
+    서명 시간까지 기록하므로 서명이 만료되게 할 수 있다.
 
-    JWS currently does not specify this behavior but it mentions a
-    possible extension like this in the spec. Expiry date is encoded
-    into the header similar to what's specified in `draft-ietf-oauth
-    -json-web-token <http://self-issued.info/docs/draft-ietf-oauth-json
-    -web-token.html#expDef>`_.
+    JWS에서는 현재 이런 동작을 명세하고 있지 않지만 이런 확장이
+    가능하다고 명세에서 언급하고 있다. `draft-ietf-oauth-json
+    -web-token <http://self-issued.info/docs/draft-ietf-oauth-json
+    -web-token.html#expDef>`_\에 명세된 것과 비슷한 방식으로
+    만료 날짜를 헤더에 인코딩 한다.
     """
 
     DEFAULT_EXPIRES_IN = 3600
